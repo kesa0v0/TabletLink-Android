aea0dbd7c439093e56c5a4ce86525970a6f7d2ca
kesa <kesa0v0@gmail.com>
2025-05-08 16:17:53 +0900

    필요없는거 다 날림 그리고 펜 입력도 그냥 머지해버림

--- app/src/main/java/com/example/tabletlink_android/MainActivity.kt
+++ app/src/main/java/com/example/tabletlink_android/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.example.tabletlink_android
 
 import android.graphics.Bitmap
+import android.media.MediaCodec
+import android.media.MediaFormat
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
+import android.view.Surface
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import io.github.thibaultbee.srtdroid.core.Srt
+import io.github.thibaultbee.streampack.core.elements.endpoints.DynamicEndpointFactory
+import io.github.thibaultbee.streampack.core.pipelines.StreamerPipeline
+import io.github.thibaultbee.streampack.ui.views.PreviewView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import net.jpountz.lz4.LZ4Factory
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
@@ -25,7 +31,6 @@ import java.nio.ByteBuffer
 import java.nio.channels.DatagramChannel
 import java.nio.channels.SelectionKey
 import java.nio.channels.Selector
-import androidx.core.graphics.createBitmap
 
 
 const val TAG = "kesa"
@@ -68,33 +73,6 @@ class Network {
         receiveChannel?.bind(null)
     }
 
-    data class FrameData(
-        var data: ByteArray,
-        var width: Int,
-        var height: Int,
-        var frameRate: Int,
-
-        var size: Int,
-        var timestamp: Long
-    )
-
-
-    fun bytesToFrameData(bytes: ByteArray): FrameData {
-        val buffer = ByteBuffer.wrap(bytes)
-
-        // 데이터 역 직렬화
-        val width = buffer.int
-        val height = buffer.int
-        val dataRate = buffer.int
-        val size = buffer.int
-        val timestamp = buffer.long
-
-        val dataLength = bytes.size - 4 * Integer.BYTES - Long.SIZE_BYTES
-        val data = ByteArray(dataLength)
-        buffer.get(data)
-
-        return FrameData(data, width, height, dataRate, size, timestamp)
-    }
 
     enum class PacketType(val id: Byte) {
         DISCOVER_TABLET_SERVER(0x01),
@@ -197,86 +175,6 @@ class Network {
         this.sendPort = port
     }
 
-    fun startListen() {
-        isConnected = true
-
-        udpJob = CoroutineScope(Dispatchers.IO).launch {
-            val selector = Selector.open()
-            receiveChannel?.register(selector, SelectionKey.OP_READ)
-
-            Log.d(TAG, "a: Start Receiving")
-            while (isConnected) {
-                try {
-                    if (selector.select(5) > 0) {
-                        val keys = selector.selectedKeys()
-                        val it = keys.iterator()
-                        while (it.hasNext()) {
-                            val key = it.next()
-                            it.remove()
-
-                            if (key.isReadable) {
-                                receiveData.clear()
-                                receiveChannel?.receive(receiveData)
-                                receiveData.flip()
-
-                                val receivedData = ByteArray(receiveData.remaining())
-                                receiveData.get(receivedData)
-
-                                val frameData = bytesToFrameData(receivedData)
-
-                                Log.d(
-                                    TAG,
-                                    "startListen: width: ${frameData.width}, height: ${frameData.height}, timestamp: ${frameData.timestamp}"
-                                )
-                                Log.d(
-                                    TAG,
-                                    "startListen: latency: ${System.currentTimeMillis() - frameData.timestamp}"
-                                )
-
-                                // 비동기 압축 해제
-                                launch(Dispatchers.Default) {
-                                    val decompressor = LZ4Factory.fastestInstance().fastDecompressor()
-//                                    val decompressedData = decompressor.decompress(frameData.data, frameData.size)
-
-                                    withContext(Dispatchers.Main) {
-//                                        xorScreen(decompressedData, frameData.width, frameData.height, frameData.timestamp)
-                                        xorScreen(frameData, frameData.width, frameData.height, frameData.timestamp)
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                } catch (e: Exception) {
-                    Log.e(TAG, "startListen: ${e.stackTraceToString()}")
-                }
-            }
-        }
-    }
-
-//    var previousFrame: ByteArray = ByteArray(1920 * 1080 * 4) // 초기화
-    private fun CoroutineScope.xorScreen(
-        screenData: FrameData,
-        width: Int,
-        height: Int,
-        timestamp: Long
-    ) {
-        Log.d(TAG, "updateScreen: width: $width, height: $height, timestamp: $timestamp")
-
-//        val newFrame = ByteArray(previousFrame.size)
-
-//        Utils.xorFrames(newFrame, previousFrame, screenData as ByteArray)
-//        previousFrame = screenData
-
-        // Bitmap 생성
-        val bmp = createBitmap(width, height)
-        bmp.copyPixelsFromBuffer(ByteBuffer.wrap(screenData.data))
-
-        surfaceView?.holder?.lockCanvas()?.let { canvas ->
-            canvas.drawBitmap(bmp, 0f, 0f, null)
-            surfaceView?.holder?.unlockCanvasAndPost(canvas)
-        }
-    }
 
     fun testSend() {
         if (sendSocket == null) {
@@ -306,14 +204,16 @@ class Network {
         udpJob = null
         sendSocket?.close()
         sendSocket = null
-        receiveChannel?.close()
-        receiveChannel = null
         Log.d(TAG, "stopListen: UDP 수신 종료")
     }
 }
 
 class MainActivity : AppCompatActivity() {
     val server = Network(InetAddress.getByName("10.0.2.2"), 12346, 12345)
+    private lateinit var surfaceView: SurfaceView
+    private lateinit var codec: MediaCodec
+    private lateinit var surface: Surface
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -324,11 +224,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        Log.d(TAG, "app started")
-
-        val surfaceView = findViewById<SurfaceView>(R.id.surfaceView)
-        val surfaceHolder = surfaceView.holder
-        server.surfaceView = surfaceView
+        Srt.
 
 
         findViewById<Button>(R.id.Discover).setOnClickListener {
@@ -342,7 +238,7 @@ class MainActivity : AppCompatActivity() {
         val s = findViewById<SwitchCompat>(R.id.switch1)
         s.setOnClickListener {
             if (s.isChecked) {
-                server.startListen()
+
             } else {
                 server.stopListen()
             }
@@ -353,6 +249,9 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         server.stopListen()
         super.onDestroy()
+        CoroutineScope(Dispatchers.IO).launch {
+            streamer.stopStream()
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
