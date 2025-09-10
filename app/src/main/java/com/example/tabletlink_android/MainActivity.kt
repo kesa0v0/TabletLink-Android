08f885340df4234fb4d5e25c44859154f269c5f6
kesa <kesa0v0@gmail.com>
2025-09-10 09:02:40 +0900

    뒤엎기전 세이브

--- app/src/main/java/com/example/tabletlink_android/MainActivity.kt
+++ app/src/main/java/com/example/tabletlink_android/MainActivity.kt
@@ -20,11 +20,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.net.DatagramPacket
-import java.net.Inet4Address
 import java.net.InetAddress
 import java.net.InetSocketAddress
-import java.net.NetworkInterface
-import java.net.SocketException
 import kotlin.concurrent.thread
 
 
